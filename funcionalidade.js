a1e0c082d04d884ed7acf13fe0465a04a1e34e04
Derick Carvalho <87207601+Darukkyh13@users.noreply.github.com>
2021-08-18 17:25:57 -0300

    Adicionei os arquivos via UpLoad

---
+++ funcionalidade.js
@@ -0,0 +1,11 @@
+function clicar1() {
+    window.location.href = "projetos/cara_a_cara/pagina_inicial/pagina_inicial.html"
+}
+
+function clicar2() {
+    window.location.href = "projetos/lista_de_tarefas/lista_de_tarefas.html"
+}
+
+function clicar3() {
+    window.location.href = "projetos/comparador_de_gpus/comparador.html"
+}
